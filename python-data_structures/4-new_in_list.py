9fe7460d197dd5320a932fe62968b6af16ed2f50
Murad Cavadov <13860@holbertonstudents.com>
2026-04-16 20:49:44 +0000

    Fix pycodestyle whitespace errors

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    # Orijinal siyahının kopyasını yaradırıq
     new_list = my_list[:]
-    
-    # İndeksi yoxlayırıq
     if idx < 0 or idx >= len(my_list):
         return new_list
-    
-    # Dəyişikliyi yalnız kopya üzərində edirik
     new_list[idx] = element
     return new_list
