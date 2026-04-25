7037bdd8177fb2f53532125ff68a92f5a6203c5c
Murad Cavadov <13860@holbertonstudents.com>
2026-04-25 19:40:47 +0000

    8. Square #1

--- python-inheritance/8-square.py
+++ python-inheritance/8-square.py
@@ -1,18 +1,25 @@
 #!/usr/bin/python3
-"""Module 8-square"""
+"""
+Module 8-square
+Defines a class Square that inherits from Rectangle
+"""
 Rectangle = __import__('7-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """Class Square"""
-    __init_subclass__ = None
+    """A class Square that inherits from Rectangle"""
 
     def __init__(self, size):
-        """Initialize"""
+        """
+        Initialize a new Square.
+
+        Args:
+            size (int): The size of the square's side.
+        """
         self.integer_validator("size", size)
         self.__size = size
         super().__init__(size, size)
 
     def area(self):
-        """Returns area"""
+        """Returns the area of the square."""
         return self.__size ** 2
