b8654ec5766b0982c038f274c02b2b717759909a
Murad Cavadov <13860@holbertonstudents.com>
2026-04-25 19:19:56 +0000

    4. Improve Geometry

---
+++ python-inheritance/4-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Module 4-base_geometry
+Defines a class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """Class BaseGeometry"""
+
+    def area(self):
+        """
+        Public instance method that raises an Exception
+        """
+        raise Exception("area() is not implemented")
