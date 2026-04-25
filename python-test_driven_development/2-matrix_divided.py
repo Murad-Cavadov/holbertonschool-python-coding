adf7007724d0e36a6b30f10db1970269d43bc806
Murad Cavadov <13860@holbertonstudents.com>
2026-04-25 18:32:55 +0000

    Add matrix_divided function and tests

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -7,31 +7,34 @@ Bu modul matrisi bolmek ucun funksiyani ehtiva edir.
 def matrix_divided(matrix, div):
     """
     Matrisin butun elementlerini div-e bolur.
+
+    Args:
+        matrix: Tam ve ya onluq ededlerden ibaret siyahilarin siyahisi.
+        div: Bolen eded (int ve ya float).
+
+    Returns:
+        Yuvarlaqlasdirilmis neticelerden ibaret yeni matris.
     """
     msg = "matrix must be a matrix (list of lists) of integers/floats"
 
-    # 1. Matrisin tipini ve daxilindeki elementlerin tipini yoxlayiriq
-    if not isinstance(matrix, list) or len(matrix) == 0:
+    # 1. Matrisin tipini ve bos olmamasini yoxlayiriq
+    if not isinstance(matrix, list) or not matrix or not all(isinstance(row, list) for row in matrix):
         raise TypeError(msg)
-    
-    for row in matrix:
-        if not isinstance(row, list) or len(row) == 0:
-            raise TypeError(msg)
-        for element in row:
-            if not isinstance(element, (int, float)):
-                raise TypeError(msg)
 
-    # 2. Setirlerin olcusunu yoxlayiriq
-    row_size = len(matrix[0])
-    for row in matrix:
-        if len(row) != row_size:
-            raise TypeError("Each row of the matrix must have the same size")
-
-    # 3. 'div' yoxlamalari
+    # 2. Div yoxlamalari
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
 
-    # 4. Yeni matris yaradiriq
+    # 3. Setirlerin eyni olcude olmasi ve elementlerin tipi
+    row_size = len(matrix[0])
+    for row in matrix:
+        if len(row) != row_size:
+            raise TypeError("Each row of the matrix must have the same size")
+        for element in row:
+            if not isinstance(element, (int, float)):
+                raise TypeError(msg)
+
+    # 4. Yeni matrisin yaradilmasi
     return [[round(x / div, 2) for x in row] for row in matrix]
