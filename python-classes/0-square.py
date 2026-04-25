787180ac0d5bddce9d06931bfd72482d87582d32
Murad Cavadov <13860@holbertonstudents.com>
2026-04-25 18:49:41 +0000

    Complete rewrite for PEP8 and documentation alignment

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-"""Defines a class Square."""
+"""Module that defines a Square class."""
 
 
 class Square:
-    """Represent a square."""
+    """A class that represents a square."""
 
     def __init__(self, size):
-        """Initialize a new square.
+        """Initialize the square with a size.
 
         Args:
-            size (int): The size of the new square.
+            size (int): The size of the square.
         """
         self.__size = size
